fed347087ecf117422efa28c7776e4fe8277464b
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-25 03:15:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,9 +26,9 @@ class Dog
   end
   
   def self.new_from_db(row)
-    @id = row[0]
-    @name = row[1]
-    @breed = row[2]
+    id = row[0]
+    name = row[1]
+    breed = row[2]
     
     
   end  
